de4a89baafe10e3383e405fc8f7f6e3d5aa8f656
tarunb12 <tbelani@ufl.edu>
2019-04-25 18:24:13 -0400

    for statement

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Type Inference Algorithm in Prolog
+
+## Testing
+```prolog
+?- [typeInf].
+?- load_test_files([]).
+?- run_tests().
+```
--- typeInf.pl
+++ typeInf.pl
@@ -1,64 +1,78 @@
-/* match functions by unifying with arguments 
-    and infering the result
-*/
+% match functions by unifying with arguments 
+% and infering the result
 typeExp(Fct, T) :-
-    \+ var(Fct), /* make sure Fct is not a variable */ 
-    \+ atom(Fct), /* or an atom */
-    functor(Fct, Fname, _Nargs), /* ensure we have a functor */
-    !, /* if we make it here we do not try anything else */
-    Fct =.. [Fname|Args], /* get list of arguments */
-    append(Args, [T], FType), /* make it loook like a function signature */
-    functionType(Fname, TArgs), /* get type of arguments from definition */
-    typeExpList(FType, TArgs). /* recurisvely match types */
+    \+ var(Fct),                    % make sure Fct is not a variable
+    \+ atom(Fct),                   % or an atom
+    functor(Fct, Fname, _Nargs),    % ensure we have a functor
+    !,                              % if we make it here we do not try anything else
+    Fct =.. [Fname|Args],           % get list of arguments
+    append(Args, [T], FType),       % make it loook like a function signature
+    functionType(Fname, TArgs),     % get type of arguments from definition
+    typeExpList(FType, TArgs).      % recurisvely match types
 
-/* propagate types */
+% propagate types
 typeExp(T, T).
 
-/* list version to allow function mathine */
+% list version to allow function matching
 typeExpList([], []).
 typeExpList([Hin|Tin], [Hout|Tout]) :-
-    typeExp(Hin, Hout), /* type infer the head */
-    typeExpList(Tin, Tout). /* recurse */
+    typeExp(Hin, Hout),         % type infer the head
+    typeExpList(Tin, Tout).     % recurse on tail
 
-/* TODO: add statements types and their type checking */
-/* global variable definition
-    Example:
-        gvLet(v, T, int) ~ let v = 3;
- */
+% gvLet(v, T, int) ~ let v = int;
 typeStatement(gvLet(Name, T, Code), unit) :-
-    atom(Name), /* make sure we have a bound name */
-    typeExp(Code, T), /* infer the type of Code and ensure it is T */
-    bType(T), /* make sure we have an infered type */
-    asserta(gvar(Name, T)). /* add definition to database */
+    atom(Name),             % make sure we have a bound name
+    typeExp(Code, T),       % infer the type of Code and ensure it is T
+    bType(T),               % make sure we have an infered type
+    asserta(gvar(Name, T)). % add definition to database
 
-/* Code is simply a list of statements. The type is 
-    the type of the last statement 
-*/
+% ifElse(Cond, TCode, FCode) ~ if (cond) { ...TCode } else { ...FCode }
+typeStatement(if(Cond, TCode, FCode), T) :- 
+    typeExp(Cond, bool),    % Check conditional type is bool
+    typeExp(TCode, T),      % Check type of code for true condition
+    typeExp(FCode, T),      % Check type of code for false condition
+    bType(T).               % Check that T is basic type
+
+% for(gvLet(Name, LetT, LetCode), Cond, Increment, ForCode) ~ for ()
+typeStatement(for(gvLet(Name, LetT, LetCode), Cond, Increment, ForCode), CodeT) :-
+    atom(Name),
+    typeExp(LetCode, LetT),
+    bType(LetT),
+    asserta(gvar(Name, LetT)),  % Add definition to database
+    typeExp(Cond, bool),        % Check conditional type is bool
+    typeExp(Increment, LetT),
+    typeExp(ForCode, CodeT),
+    deleteGVars().              % Remove definitions from database
+
+% typeStatement(fctLet(Name, T, TCode, S, Code), S).
+
+% Code is simply a list of statements. The type is 
+% the type of the last statement 
+
 typeCode([S], T) :- typeStatement(S, T).
 typeCode([S, S2|Code], T) :-
     typeStatement(S,_T),
     typeCode([S2|Code], T).
 
-/* top level function */
+% top level function
 infer(Code, T) :-
-    is_list(Code), /* make sure Code is a list */
-    deleteGVars(), /* delete all global definitions */
+    is_list(Code),      % make sure Code is a list
+    deleteGVars(),      % delete all global definitions
     typeCode(Code, T).
 
-/* Basic types
-    TODO: add more types if needed
- */
-bType(int).
-bType(float).
-bType(string).
-bType(unit). /* unit type for things that are not expressions */
+% Basic types
+bType(int).                         % Basic integer type
+bType(float).                       % Basic float type
+bType(string).                      % Basic string type
+bType(bool).                        % Basic boolean type
+bType(unit).                        % Basic unit type (non-expressions)
+bType([H]) :- bType(H).             % Basic function type 'a ([a])
+bType([H|T]) :- bType(H), bType(T). % Basic function type 'a -> ... -> 'z ([a, ..., z])
 /*  functions type.
     The type is a list, the last element is the return type
     E.g. add: int->int->int is represented as [int, int, int]
     and can be called as add(1,2)->3
  */
-bType([H]) :- bType(H).
-bType([H|T]) :- bType(H), bType(T).
 
 /*
     TODO: as you encounter global variable definitions
@@ -76,34 +90,33 @@ bType([H|T]) :- bType(H), bType(T).
     variables. Best wy to do this is in your top predicate
 */
 
-deleteGVars() :- retractall(gvar), asserta(gvar(_X,_Y):-false()).
+deleteGVars() :- retractall(gvar), asserta(gvar(_X, _Y) :- false()).
 
-/*  builtin functions
-    Each definition specifies the name and the 
-    type as a function type
-
-    TODO: add more functions
-*/
-
-fType(iplus, [int,int,int]).
-fType(fplus, [float, float, float]).
-fType(fToInt, [float,int]).
-fType(iToFloat, [int,float]).
-fType(print, [_X, unit]). /* simple print */
+% Built in functions
+fType(iplus, [int, int, int]).          % (int + int) -> [int]
+fType(fplus, [float, float, float]).    % (float + float) -> [float]
+fType(fToInt, [float, int]).            % float -> int (conversion)
+fType(iToFloat, [int, float]).          % int -> float (conversion)
+fType(lessThan, [int, int, bool]).      % (int > int) -> bool
+fType(greaterThan, [int, int, bool]).   % (int < int) -> bool
+fType(equal, [int, int, bool]).         % (int = int) -> bool
+fType(notEqual, [int, int, bool]).      % (int ≠ int) -> bool
+fType(and,[bool, bool, bool]).          % (bool && bool) -> bool
+fType(or, [bool, bool, bool]).          % (bool || bool) -> bool
+fType(print, [_X, unit]).               % simple print
 
 /* Find function signature
-   A function is either buld in using fType or
+   A function is either built in using fType or
    added as a user definition with gvar(fct, List)
 */
 
 % Check the user defined functions first
 functionType(Name, Args) :-
     gvar(Name, Args),
-    is_list(Args). % make sure we have a function not a simple variable
+    is_list(Args).      % make sure we have a function, not a simple variable
 
 % Check first built in functions
 functionType(Name, Args) :-
-    fType(Name, Args), !. % make deterministic
+    fType(Name, Args), !.   % make deterministic
 
-% This gets wiped out but we have it here to make the linter happy
 :- dynamic(gvar/2).
